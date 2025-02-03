32a2bfc0f181f40193bcef3fb4d7f18b56010e0d
ZoieV2025 <zoie.vitale2025@gbwl.org>
2025-02-03 14:39:46 +0000

    updated java.main

--- Main.java
+++ Main.java
@@ -27,6 +27,33 @@ public class Main {
       // Use the .add( obj ) METHOD to populate/fill list
       testScores.add(99.50);
       System.out.println(testScores); // print [95.5]
+      
+      // 7.2: ArrayList METHODS
+      // after "new", make call to the CONSTRUCTOR
+      ArrayList<String> toBuy = new ArrayList<String>(); 
+
+      // .add(obk) appends obj to END OF LIST
+      toBuy.add("frozen mango");
+      toBuy.add("frozen pinapple");
+      toBuy.add("frozen strawberries");
+      toBuy.add("ninja blender");
+      toBuy.add("vitamin C gummies");
+      System.out.println("SHOPPING LIST: ");
+      System.out.println(toBuy);
+
+      // .size() returns in -> number of items
+      System.out.println(toBuy.size() + " things left to buy!");
+
+      // .add(int index, obj) -> "INSERT" at index 
+      toBuy.add(0, "Extra Mint Gum");
+      System.out.println(toBuy);
+      // ALSO shifts over elements from that index on
+      
+      // .get(int index) returns an object of the same type as list holds
+      // int lastIndex = toBuy.size() - 1;
+      String lastElement = toBuy.get(lastIndex);
+      System.out.println(lastElement);
+      System.out.println(toBuy.get(3));
 
 
 
